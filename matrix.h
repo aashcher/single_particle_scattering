035939c04203f47af488dceb81392620e38d3898
Alexey A. Shcherbakov <47977458+aashcher@users.noreply.github.com>
2022-12-12 22:19:58 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,3 +1,20 @@
+/*
+
+Copyright © 2022 Alexey A. Shcherbakov. All rights reserved.
+
+This file is part of the SingleParticleScattering project.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This sortware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
 
 #ifndef _MATRIX_H
 #define _MATRIX_H
